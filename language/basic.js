28a503c07b52fdfa7f47fe94989dca8802738225
guane <Ethan.GuangxueCao@pega.com>
2018-10-24 11:01:35 +0000

    sync

---
+++ language/basic.js
@@ -0,0 +1,10 @@
+const o = {
+    a: 1,
+    b: 2,
+    c: 3
+}
+
+// define variable a, b,c and their value equals to value that has the same key in object o;
+const {a, b, c} = o;
+
+// a = 1,  b = 2, c = 3;
--- todo-react-redux/src/container/FilterLink.js
+++ todo-react-redux/src/container/FilterLink.js
@@ -2,12 +2,12 @@ import Link from "../components/Link";
 import {connect} from  "react-redux";
 import {setVisibilityFilter} from "../actions";
 
-const mapStateToProps = (state, ownProps) => {
+const mapStateToProps = (state, ownProps) => ({
     active : ownProps.filter == state.visibilityFilter
-};
+});
 
-const mapDispatchToProps = (dispatch, ownProps) => {
+const mapDispatchToProps = (dispatch, ownProps) => ({
     onclick : () => dispatch(setVisibilityFilter(ownProps.filter))
-}
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Link);
